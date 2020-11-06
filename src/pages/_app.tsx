a60704c8a11b8cb1faa8af926a78f5f9479b46e6
Thales de Oliveira <thalesoliveira200@gmail.com>
2020-11-06 17:11:04 -0300

    feat: add commit lint and eslint to CI step

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import '../assets/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {

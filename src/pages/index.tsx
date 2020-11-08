f34aca1945b683c90521532935be099a6091bda3
Thales de Oliveira <thalesoliveira200@gmail.com>
2020-11-08 16:42:26 -0300

    feat: add global styles to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { GlobalStyle } from '../themes/GlobalStyle';
 // import Head from 'next/head';
 // import styles from '../assets/styles/Home.module.css';
 
 export default function Home() {
-  return <div>Ola mundo</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>Ola mundo</div>
+    </>
+  );
 }

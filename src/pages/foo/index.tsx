02aecf1d9b138edb51b1e1e3d0ac89f5e3231644
Thales de Oliveira <thalesoliveira200@gmail.com>
2020-11-20 13:38:56 -0300

    feat: add antd to project

---
+++ src/pages/foo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Botao } from './styles';
+
+const Foo: React.FC = () => {
+  return (
+    <div>
+      <Botao>Blabla</Botao>
+    </div>
+  );
+};
+
+export default Foo;

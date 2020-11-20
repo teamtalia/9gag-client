5584516fa61b9e2bffeca5eddf5aca7e05962b9a
Thales de Oliveira <thalesoliveira200@gmail.com>
2020-11-20 14:16:17 -0300

    fix: fix Page Without Valid React Component on deploy

---
+++ src/pages/foo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Foo from '../views/foo';
+
+// import { Container } from './styles';
+
+const FooPage: React.FC = () => {
+  return <Foo />;
+};
+
+export default FooPage;

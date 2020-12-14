c6df9eb3211b2192f6c4d7a9ddfb9f319c2d6cda
Thales de Oliveira <thalesoliveira200@gmail.com>
2020-12-14 10:50:28 -0300

    fix: fix base_url api

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,4 @@ export const MOBILE_APP_URL = 'https://expo.io/@thales_dev/projects/Talia';
 export const baseURL =
   process.env.NODE_ENV === 'development'
     ? `http://localhost:5000`
-    : `https://api.taliaap.co/`;
+    : `https://api.taliaapp.co/`;
